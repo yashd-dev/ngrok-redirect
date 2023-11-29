7c7dd85403b29d6f252c61f636db2575842eac18
Yash <78804783+yashd-dev@users.noreply.github.com>
2023-11-30 03:04:47 +0530

    Add tailwindcss integration and update dependencies

---
+++ tailwind.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				rosePine: {
+					base: "#191724",
+					surface: "#1f1d2e",
+					overlay: "#26233a",
+					muted: "#6e6a86",
+					subtle: "#908caa",
+					text: "#e0def4",
+					love: "#eb6f92",
+					gold: "#f6c177",
+					rose: "#ebbcba",
+					pine: "#31748f",
+					foam: "#9ccfd8",
+					iris: "#c4a7e7",
+					highlightLow: "#21202e",
+					highlightMed: "#403d52",
+					highlightHigh: "#524f67",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
